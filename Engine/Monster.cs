effca8294261416313d5479c582988f0bdd6cd54
Satan_2 <Satan_2@KURANOSUKE>
2016-06-19 21:39:15 -0400

    Add project files.

---
+++ Engine/Monster.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Engine
+{
+    public class Monster : LivingCreature
+    {
+        public int ID { get; set; }
+        public string Name { get; set; }
+        public int MaximumDamage { get; set; }
+        public int RewardExperiencePoints { get; set; }
+        public int RewardGold { get; set; }
+        public List<LootItem> LootTable { get; set; }
+
+        public Monster(int id, string name, int maximumdamage, int rewardexperiencepoints, int rewardgold, int currenthitpoints, int maximumhitpoints) : base(currenthitpoints,maximumhitpoints)
+        {
+            ID = id;
+            Name = name;
+            MaximumDamage = maximumdamage;
+            RewardExperiencePoints = rewardexperiencepoints;
+            RewardGold = rewardgold;
+            LootTable = new List<LootItem>();
+        }
+    }
+}
